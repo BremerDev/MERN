b9635112037e1f78e47798cf570d2fa1225ff7fc
Jesse Bremer <jsr.bremer@gmail.com>
2024-03-05 12:39:20 -0500

    Groundwork for frontend to listen to backend.

--- todos-front-end/src/App.js
+++ todos-front-end/src/App.js
@@ -1,8 +1,40 @@
+import { useState, useEffect } from 'react';
+import { NewToDoForm } from './NewToDoForm';
+import { ToDoList } from './ToDoList';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      // ...
+    }
+
+    loadTodos();
+  }, []);
+
+  const createTodo = todoText => {
+    // ...
+  }
+
+  const completeTodo = todoId => {
+    // ...
+  }
+
+  const deleteTodo = todoId => {
+    // ...
+  }
+
   return (
-    <h1>ToDo</h1>
+    <div>
+      <h1>ToDo</h1>
+      <NewToDoForm onClickCreate={createTodo} />
+      <ToDoList
+        todos={todos}
+        onCompleteTodo={completeTodo}
+        onDeleteTodo={deleteTodo} />
+    </div>
   );
 }
 
